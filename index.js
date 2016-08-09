6c958a4ef1287752a134d7d367cd816b55d41ae2
K-Calderon-ASC <calderonkevin108@gmail.com>
2016-08-09 10:52:13 -0400

    x

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-$(".homeslideshow").click( function(){
-	$("#picshow").animate({
-		opacity: 0
-	}, 1500);
-	$("#picshow").attr("src", "http://media.mercola.com/assets/images/food-facts/banana-fb.jpg");
-	console.log("okay"),
-	$("#picshow").delay(3000);	
-	$("#picshow").animate({
-		opacity: 100
-	}, 1500);
+srcLinks = ["http://fitnessandhealthadvisor.com/wp-content/uploads/2013/05/bananas1.jpeg","http://media.mercola.com/assets/images/food-facts/banana-fb.jpg", "https://upload.wikimedia.org/wikipedia/commons/4/44/Bananas_white_background_DS.jpg", "https://upload.wikimedia.org/wikipedia/commons/4/4c/Bananas.jpg"];
+$("#circle1").click( function(){
+	$("#picshow")
+		.fadeOut(1000, function(){
+			$("#picshow").attr("src", srcLinks[0]);
+	})
+		.fadeIn(1000);
+});
+$("#circle2").click( function(){
+	$("#picshow")
+		.fadeOut(1000, function(){
+			$("#picshow").attr("src", srcLinks[1]);
+	})
+		.fadeIn(1000);
+});
+$("#circle3").click( function(){
+	$("#picshow")
+		.fadeOut(1000, function(){
+			$("#picshow").attr("src", srcLinks[2]);
+	})
+		.fadeIn(1000);
+});
+$("#circle4").click( function(){
+	$("#picshow")
+		.fadeOut(1000, function(){
+			$("#picshow").attr("src", srcLinks[3]);
+	})
+		.fadeIn(1000);
 });
