335d10c61ee90a0737d015beece922fb82091cc8
K-Calderon-ASC <calderonkevin108@gmail.com>
2016-08-09 09:29:33 -0400

    49

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ $(".homeslideshow").click( function(){
 	$("#picshow").animate({
 		opacity: 0
 	}, 1500);
+	$("#picshow").attr("src", "http://media.mercola.com/assets/images/food-facts/banana-fb.jpg");
+	$("#picshow").animate({
+		opacity: 0
+	}, 1500);
 });
