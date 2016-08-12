352e176834c90dd72e2c5147511c4ef39eb5e370
K-Calderon-ASC <calderonkevin108@gmail.com>
2016-08-12 10:09:19 -0400

    d

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var i = 0;
-srcLinks = ["https://secure.static.tumblr.com/dc40823055c62742b840a1f36fe3a059/rse8v7p/Ckfnrb4gf/tumblr_static__640_v2.gif","http://25.media.tumblr.com/99cbcdc1b112fa1fad105fd5bcec77eb/tumblr_mz3r7taEcg1rqess5o1_500.gif", "https://media4.giphy.com/media/dh6120qvBRyda/200_s.gif"];
+srcLinks = ["https://secure.static.tumblr.com/dc40823055c62742b840a1f36fe3a059/rse8v7p/Ckfnrb4gf/tumblr_static__640_v2.gif","http://25.media.tumblr.com/99cbcdc1b112fa1fad105fd5bcec77eb/tumblr_mz3r7taEcg1rqess5o1_500.gif", "https://media4.giphy.com/media/dh6120qvBRyda/200_s.gif", "http://67.media.tumblr.com/ca1d7c1d12576587b2f18e4c32280c54/tumblr_mnw4cjrJuV1s3igp0o1_500.gif"];
 $("#circle1").click( function(){
 	i=0;
 	$("#picshow")
@@ -50,4 +50,4 @@ $("#circle4").click( function(){
 	})
 		.fadeIn(1000);
 			}
-		   }, 5000);
+		   }, 100000);
