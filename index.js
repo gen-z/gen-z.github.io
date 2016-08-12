ef6f7c7f0863eb0103ef492981cd55ae4ccb5bec
K-Calderon-ASC <calderonkevin108@gmail.com>
2016-08-12 14:30:27 -0400

    rv

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 var i = 0;
 srcLinks = ["http://media0.giphy.com/media/lKXEBR8m1jWso/giphy.gif","https://s3.amazonaws.com/wp-ag/wp-content/uploads/sites/72/2015/04/ClintonShruggiegif.gif", "http://img.ffffound.com/static-data/assets/6/8e3f5db6667474cf5430c5fc392dd3f3f1105abb_m.gif", "http://67.media.tumblr.com/ca1d7c1d12576587b2f18e4c32280c54/tumblr_mnw4cjrJuV1s3igp0o1_500.gif"];
-srcText=["BrainWaves/#Editorials", "Politics/#Hillary","Art/#Modern", "Sports/#Skatebaording"]
+srcText=["BrainWaves/#Editorials", "Politics/#Hillary","Art/#Modern", "Sports/#Skateboarding"];
+channelLink = ["sub-pages/channels/brain-waves/editorials.html","sub-pages/channels/uspol/hillary.html","sub-pages/channels/art/modern.html","sub-pages/channels/sports/skateboard.html"];
+
 $("#circle1").click( function(){
 	i=0;
 	$(".homeslideshow")
 		.fadeOut(1000, function(){
 			$("#picshow").attr("src", srcLinks[i]);
 			$("#slideshowName").html(srcText[i]);
+			$(".channelLink").attr("href", channelLink[i]);
 	})
 		.fadeIn(1000);
 });
@@ -16,6 +19,7 @@ $("#circle2").click( function(){
 		.fadeOut(1000, function(){
 			$("#picshow").attr("src", srcLinks[i]);
 			$("#slideshowName").html(srcText[i]);
+			$(".channelLink").attr("href", channelLink[i]);
 	})
 		.fadeIn(1000);
 });
@@ -25,6 +29,7 @@ $("#circle3").click( function(){
 		.fadeOut(1000, function(){
 			$("#picshow").attr("src", srcLinks[i]);
 			$("#slideshowName").html(srcText[i]);
+			$(".channelLink").attr("href", channelLink[i]);
 	})
 		.fadeIn(1000);
 });
@@ -34,6 +39,7 @@ $("#circle4").click( function(){
 		.fadeOut(1000, function(){
 			$("#picshow").attr("src", srcLinks[i]);
 			$("#slideshowName").html(srcText[i]);
+			$(".channelLink").attr("href", channelLink[i]);
 	})
 		.fadeIn(1000);
 });
@@ -45,6 +51,7 @@ $("#circle4").click( function(){
 					.fadeOut(1000, function(){
 			$("#picshow").attr("src", srcLinks[i]);
 			$("#slideshowName").html(srcText[i]);
+			$(".channelLink").attr("href", channelLink[i]);
 	})
 		.fadeIn(1000);
 			}
@@ -54,6 +61,7 @@ $("#circle4").click( function(){
 		.fadeOut(1000, function(){
 			$("#picshow").attr("src", srcLinks[i]);
 			$("#slideshowName").html(srcText[i]);
+			$(".channelLink").attr("href", channelLink[i]);
 	})
 		.fadeIn(1000);
 			}
