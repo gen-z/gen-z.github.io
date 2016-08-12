b94058573627b32873dd694b6a78ba6d9689adec
K-Calderon-ASC <calderonkevin108@gmail.com>
2016-08-12 14:09:54 -0400

    dv

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var i = 0;
-srcLinks = ["https://secure.static.tumblr.com/dc40823055c62742b840a1f36fe3a059/rse8v7p/Ckfnrb4gf/tumblr_static__640_v2.gif","http://25.media.tumblr.com/99cbcdc1b112fa1fad105fd5bcec77eb/tumblr_mz3r7taEcg1rqess5o1_500.gif", "https://media4.giphy.com/media/dh6120qvBRyda/200_s.gif", "http://67.media.tumblr.com/ca1d7c1d12576587b2f18e4c32280c54/tumblr_mnw4cjrJuV1s3igp0o1_500.gif"];
-
+srcLinks = ["http://media0.giphy.com/media/lKXEBR8m1jWso/giphy.gif","https://s3.amazonaws.com/wp-ag/wp-content/uploads/sites/72/2015/04/ClintonShruggiegif.gif", "http://img.ffffound.com/static-data/assets/6/8e3f5db6667474cf5430c5fc392dd3f3f1105abb_m.gif", "http://67.media.tumblr.com/ca1d7c1d12576587b2f18e4c32280c54/tumblr_mnw4cjrJuV1s3igp0o1_500.gif"];
+srcText=["Brain Waves/#Editorials", "Politics/#Hillary","Art/#Modern", "Sports/#Skatebaording"]
 $("#circle1").click( function(){
 	i=0;
 	$(".homeslideshow")
